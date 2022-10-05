a5b801b9a96f72d99d967b12539f430ef5a27a2f
Gabriel Maguna <gabimaguna@gmail.com>
2022-10-05 16:23:26 -0300

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,10 @@
-
-
-export default function Home(){
-    return (
-        <><div>Hola , este es el Home!!</div></>
-    )
+export default function Home() {
+  return (
+    <>
+        <img src="" alt="Imagen" />
+      <div>Hola , este es el Home!!</div>
+      <h2>Prueba de gabi</h2>
+      <p>Esperemos q tire error</p>
+    </>
+  );
 }
