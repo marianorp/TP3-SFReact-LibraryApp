772292b3c979e4677f17524ae600b676e4897d7a
Gabriel Maguna <89588344+Gabrielmaguna00@users.noreply.github.com>
2022-10-05 16:28:13 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,7 @@ export default function Home(){
     return (
         <>
         <h1>Home</h1>
-        <span>Tira error!</span>
-        <div>Hola , este es el Home!!</div>
+        <h2>Home</h2>
         </>
     )
 }
