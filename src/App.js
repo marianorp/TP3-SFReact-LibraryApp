e81e8a933184059e44b191505a5b85313fd9f8ee
[marianorp] <[mariano.rperez26@gmail.com]>
2022-10-18 12:39:02 -0300

    fix contact us

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import {BrowserRouter , Routes, Route} from 'react-router-dom'
+import "./App.css";
 
 //Pages
-
 import Contact from './components/Contact';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
 import Register from './components/Register';
 import User from './components/User';
 import AddBook from './components/AddBook';
-import "./App.css";
 import BooksList from './components/BooksList';
 import Landing from './components/Landing';
-
 
 
 function App() {
