5c550b9d176be742cdbf4a1e8c61c71f280092fa
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2022-10-19 18:43:31 -0300

    Fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import styles from "./styles/Navbar.module.css";
 import { Link } from "react-router-dom";
 
-export default function Navbar(){
-    
-    return (
-        <div className={styles.navbar}> 
-            <Link className={styles['nav-logo']} to="/">Library App!</Link>    
-            <div className={styles['nav-items']}>
-                <Link to="/contact">Contact Us</Link>
-            </div>    
-        </div>
-    )
+import { useNavigate } from "react-router-dom";
+import SearchBar from "./SearchBar";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+  const handleCreate = (e) => {
+    e.preventDefault();
+    navigate("/addbook");
+  };
+
+  return (
+    <div className={styles.navbar}>
+      <Link className={styles["nav-logo"]} to="/">
+        Library App!
+      </Link>
+      <SearchBar />
+      <button onClick={(e) => handleCreate(e)}>Add Book</button>
+      <div className={styles["nav-items"]}>
+        <Link to="/contact">Contact Us</Link>
+      </div>
+    </div>
+  );
 }
